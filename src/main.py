43f4d23993d554e52fc3ba056d2b3d11bf48aa72
Amine El Kouhen <amine.elkouhen@cockroachlabs.com>
2025-12-16 16:53:07 +0100

    adding tools references

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import sys
 import click
 from src.common.config import parse_crdb_uri, set_crdb_config_from_cli
 from src.common.server import mcp
+import src.tools.cluster_monitoring
+import src.tools.database_operations
+import src.tools.table_management
+import src.tools.query_engine
 
 class CockroachMCPServer:
     def __init__(self):
